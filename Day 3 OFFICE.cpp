13592fac7acb77fbe3dcf36de857e05f6529193a
Chetna Bharti <145329164+Chetna3110@users.noreply.github.com>
2024-09-30 18:20:17 +0530

    day 3_office done

---
+++ Day 3 OFFICE.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int X, Y, t;
+    cin >> t;  
+
+    while(t--) {
+        cin >> X >> Y;  
+        cout << (4 * X) + Y << endl;  
+    }
+
+    return 0;
+}
