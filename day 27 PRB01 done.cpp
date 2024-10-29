8851d8563b8d8202f772146ae15b83de572d7c47
Chetna Bharti <145329164+Chetna3110@users.noreply.github.com>
2024-10-29 16:09:02 +0530

    day 27 PRB01 done

---
+++ day 27 PRB01 done.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int count=0;
+      
+        for(int i=1;i<=n;i++){
+            if(n%i==0)
+                count++;
+        }
+        count!=2? cout<<"no\n" :cout<<"yes\n";
+    }
+
+	
+}
