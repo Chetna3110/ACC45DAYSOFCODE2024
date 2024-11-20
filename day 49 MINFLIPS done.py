b7adef87ea3bc5abf846cf619a1af52ff4a64efc
Chetna Bharti <145329164+Chetna3110@users.noreply.github.com>
2024-11-20 17:55:30 +0530

    day 49 MINFLIPS done

---
+++ day 49 MINFLIPS done.py
@@ -0,0 +1,11 @@
+# cook your dish here
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    t_sum = sum(a)
+    
+    if n % 2 == 0:
+        print(abs(t_sum) // 2)
+    else:
+        print("-1")
